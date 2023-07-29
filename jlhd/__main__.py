6ca4e1a733ba67f64a1dcccb4615e3347174844a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-07-29 10:29:08 -0400

    Implement simple generator app

---
+++ jlhd/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+from jlhd.docset import Docset
+
+parser = argparse.ArgumentParser(
+    prog="jlhd",
+    description="Fetch Documenter.jl websites and pack them into DocSet",
+)
+parser.add_argument("bundle_id")
+parser.add_argument("bundle_name")
+parser.add_argument("platform_family")
+parser.add_argument("url")
+
+parser.add_argument("--allow_js", action="store_true")
+parser.add_argument("--playground")
+parser.add_argument("--fallback_url")
+
+args = parser.parse_args()
+bundle_id = args.bundle_id
+bundle_name = args.bundle_name
+platform_family = args.platform_family
+url = args.url
+
+docset = Docset(
+    bundle_id,
+    bundle_name,
+    platform_family,
+    url,
+    allow_js=args.allow_js,
+    playground=args.playground,
+    fallback_url=args.fallback_url,
+)
+
+docset.render()
