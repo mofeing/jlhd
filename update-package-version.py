e0661597f5f52c8db3ab5bb1009639b9d6a51a6d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-08-05 01:49:45 +0200

    Bump version on PR creation

---
+++ update-package-version.py
@@ -0,0 +1,12 @@
+import argparse
+import tomlkit as toml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("package")
+parser.add_argument("version")
+args = parser.parse_args()
+
+with open("packages.toml", "r") as fh:
+    pkgs = toml.load(fh)
+
+pkgs[args.package]['builds'].append(args.version)
